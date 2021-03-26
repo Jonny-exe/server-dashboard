34d2dd0018b39aaa909ff498ddf73ef521f5a2b0
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-03-26 23:51:49 +0100

    added some styling and more stats.

--- index.php
+++ index.php
@@ -1,42 +1,49 @@
 <body>
-<?php
-
-exec("ps -e -o pid,cmd,%cpu,%mem --sort=-%cpu | head -n 10 | tabulate -1 -f github | cut -f 2- -d '|' | sed '2s/----/    /'", $output);
-// exec("ps -e -o pid,cmd,%cpu,%mem --sort=-%cpu | tabulate -1 -f github | cut -f 2- -d '|' | sed '2s/----/    /'", $output);
-exec("landscape-sysinfo", $sysinfo);
-exec('CELSIUS=$(cat /sys/class/thermal/thermal_zone0/temp) && echo "scale=2; $CELSIUS/1000" | bc | tr "\n" " " && echo "Celsius"', $temperature);
-
-print "<link rel='stylesheet' href='styles.css'>";
-
-?>
-<div id="usage">
+    <link rel="stylesheet" href="https://unpkg.com/charts.css/dist/charts.min.css">
     <?php
-    print "<h1> Programs usage </h1>";
-    foreach ($output as $o) {
-        print $o;
-        print "<br>";
-    }
-    ?>
-</div>
 
-<div id="sysinfo">
-    <?php
-    print "<h1> Sysinfo </h1>";
-    foreach ($sysinfo as $o) {
-        print $o;
-        print "<br>";
+    function print_response($response, $div_name, $title)
+    {
+        print "<div id=$div_name class='grid-item'>";
+        print "<h1> $title </h1>";
+        foreach ($response as $line) {
+            print $line;
+            print "<br>";
+        }
+        print "</div>";
     }
 
-    ?>
-</div>
-<div id="temperature">
-    <?php
-    print "<h1> Cpu Temperature </h1>";
-    foreach ($temperature as $o) {
-        print $o;
-        print "<br>";
+    function print_chart($data)
+    {
+        print '<table class="charts-css area" id="my-chart"> <tbody>';
+        foreach ($data as $line) {
+            print <<<EOD
+                <tr>
+                    <td style="--start: 0.2; --size: 0.4"> <span class="data"> $ 40K </span> </td>
+                </tr>
+            EOD;
+        }
+
+
+        print "</tbody> </table>";
     }
 
+    exec("ps -e -o pid,cmd,%cpu,%mem --sort=-%cpu | head -n 10 | tabulate -1 -f github | cut -f 2- -d '|' | sed '2s/----/    /'", $usage);
+    // exec("top | head", $usage);
+    // exec("ps -e -o pid,cmd,%cpu,%mem --sort=-%cpu | tabulate -1 -f github | cut -f 2- -d '|' | sed '2s/----/    /'", $output);
+    exec("landscape-sysinfo", $sysinfo);
+    exec('CELSIUS=$(cat /sys/class/thermal/thermal_zone0/temp) && echo "scale=2; $CELSIUS/1000" | bc | tr "\n" " " && echo "Celsius"', $temperature);
+    exec('/etc/update-motd.d/90-updates-available', $upgradable);
+    exec('/etc/update-motd.d/98-reboot-required', $reboot);
+    exec('/etc/update-motd.d/00-header', $title);
+
+    print "<link rel='stylesheet' href='styles.css'>";
+    print_response($title, "title", "Your server");
+    print_response($usage, "usage", "Programs usage");
+    print_response($sysinfo, "sysinfo", "System info");
+    print_response($upgradable, "upgradable", "Apts to be upgraded");
+    print_response($reboot, "reboot", "Reboot required");
+
     ?>
-</div>
+
 </body>
